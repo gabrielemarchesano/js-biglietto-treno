fe308a4e4be53e9315177e6ecebd6bad6c37fcea
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-06 14:08:48 +0200

    debugging the instructions to do

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+il prezzo del biglietto è definito in base ai km (0.21 € al km),
+va applicato uno sconto del 20% per i minorenni,
+va applicato uno sconto del 40% per gli over 65.,
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.,
+
+TOOLS:
+- variabili let/const
+- prompt
+- condizioni if/else
+- operatori relazionali e logici
+*/
+
+// 1. Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+  // 1.1 Creo una variabile per i chilometri
+  // 1.2 Creo una variabile per l'età del passeggero
+
+// 2 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
+  // 2.1 Calcolo il prezzo del biglietto in base ai km (0.21€ al km)
+  // 2.2 Verifico se l'età del passeggero è sotto i 18 anni
+    // 2.2.1 Se è sotto i 18 anni, calcolo lo sconto del 20% al biglietto e lo stampo
+  // 2.3 Verifico se l'età del passeggero è sopra i 65 anni
+    // 2.3.1 Se è sopra i 65 anni, applico lo sconto del 40% al biglietto e lo stampo
+  // 2.4 Se il passeggero non è minorenne e non è over 65, stampo il prezzo non scontato del biglietto
+
+  // 3 Stampo biglietto
